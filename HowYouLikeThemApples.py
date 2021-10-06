668f6ed72ead7553ab2e5537375240347c6fd599
Mauro-Hendriks <33409@ma-web.nl>
2021-10-06 12:48:26 +0200

    appels

---
+++ HowYouLikeThemApples.py
@@ -0,0 +1,7 @@
+bomenz= 333/3*1
+bomens= 333/3*2
+productiez= 146
+producties= 146/100*80
+appelss= bomenz*productiez
+appelsz= bomens*producties
+print("totaal aantal appels die ik kan verkoopen: " + str(round(appelss+appelsz/3 )))
