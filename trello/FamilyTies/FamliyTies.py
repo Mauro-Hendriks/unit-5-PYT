c62fc4c5590fb0dfec136f15861a83a533227812
Mauro-Hendriks <33409@ma-web.nl>
2021-10-13 11:31:22 +0200

    new map

---
+++ trello/FamilyTies/FamliyTies.py
@@ -0,0 +1,6 @@
+naam= ("Mauro Hendriks")
+leeftijd=("16")
+km=("23,4")
+woon=("Purmerend")
+
+print("Mijn naam is "+naam+", ik ben "+leeftijd+" jaar oud en woon in "+woon+". Ik woon "+km+" kilometer van het Mediacollege.")
