4a69e769e092b5c51e5daaf4905c43ff4cdce3e5
Mauro-Hendriks <33409@ma-web.nl>
2021-10-13 11:37:52 +0200

    weekdagen

---
+++ trello/SchoolTool/SchoolTool.py
@@ -0,0 +1,23 @@
+weekdag= ("donderdag")
+
+if weekdag == ("maandag"):
+    print("pak je spullen je moet naar school")
+
+if weekdag == ("dinsdag"):
+    print("pak je spullen je moet naar school")
+
+if weekdag == ("woensdag"):
+    print("pak je spullen je moet naar school")
+
+if weekdag == ("donderdag"):
+    print("pak je spullen je moet naar school")
+
+if weekdag == ("vrijdag"):
+    print("pak je spullen je moet naar school")
+
+if weekdag == ("zaterdag"):
+    print("het is weekend je kan uit slapen")
+
+if weekdag == ("zondag"):
+    print("het is weekend je kan uit slapen")
+   
