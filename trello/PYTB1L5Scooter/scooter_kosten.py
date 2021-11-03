3c17eaf68392018a938f917cbf8fc5ec65c0acc3
Mauro-Hendriks <33409@ma-web.nl>
2021-11-03 11:48:51 +0100

    added PYTB1L5Scooter

--- trello/PYTB1L5Scooter/scooter_kosten.py
+++ trello/PYTB1L5Scooter/scooter_kosten.py
@@ -1,19 +1,30 @@
-invoer = "";
 
-# Zolang de invoer geen float is, wordt deze while loop uitgevoerd
-while not isinstance(invoer, float):
-    verzekering_per_maand = 23
-    benzine_kosten_per_liter = 1.54
-    liter_per_kilometer = 0.2 
+verzekering_per_maand = 23.0
+benzine_kosten_per_liter = 1.54
+liter_per_kilometer = 0.2
 
+km_per_maand = "";
+
+
+
+
+
+while not isinstance(km_per_maand, float):
+
+      
     try:
-        km_per_maand = input("Hoeveel KM per maand rij je: ")
         
-        invoer = float(invoer)
+        
+        km_per_maand = input("hoe veel km rij je per maand: ")
+        
+        km_per_maand = float(km_per_maand)
 
-        print(str(maandkosten))
+        maandkosten = (km_per_maand * liter_per_kilometer * benzine_kosten_per_liter) + verzekering_per_maand
+        
+        print("dit zijn je maandlijkse kosten:" + str(round(maandkosten, 2)))
 
     except ValueError:
-        print(invoer + " is geen geldig getal!")
+        
+        print(km_per_maand + " is geen geldig getal!")
 
-    maandkosten = (km_per_maand * liter_per_kilometer * benzine_kosten_per_liter) + verzekering_per_maand  
+      
